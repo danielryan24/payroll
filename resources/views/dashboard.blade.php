f6c98198ae1e9cd6e13b7ff36d4f67bc11e1d564
Daniel Ryan Simatupang <danielryan240202@gmail.com>
2023-07-29 15:41:16 +0700

    dashboard done

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>Dashboard</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+  </head>
+  <body>
+<section>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container">
+      <a class="navbar-brand" href="/">PT. CITRA CAHAYA MANDIRI</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Master Data
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Data Pegawai</a></li>
+              <li><a class="dropdown-item" href="#">Data Jabatan</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Data Gaji</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Laporan
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Laporan Gaji</a></li>
+            <li><a class="dropdown-item" href="#">Slip Gaji</a></li>
+          </ul>
+        </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</section>
+<section>
+  <div class="container">
+    <h1>Dashboard</h1>
+  </div>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Data Pegawai</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">2</h6>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Data Admin</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">1</h6>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Data Jabatan</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">4</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</section>
+  </body>
+</html>
